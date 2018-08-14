aa89d10c8d0f71140e2e8d7179cedeb66da9b436
Nathan Jangula <njangula@myriadmobile.com>
2018-08-14 08:50:22 -0500

    added string extension for regex matching

--- BetterKit/Classes/StringExtensions.swift
+++ BetterKit/Classes/StringExtensions.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+extension String {
+    public func matches(pattern: String, options: NSRegularExpression.Options = [], matchingOptions: NSRegularExpression.MatchingOptions = []) -> Bool {
+        var expression: NSRegularExpression!
+        do {
+            expression = try NSRegularExpression(pattern: pattern, options: options)
+        } catch {
+            print("Invalid pattern: %@\n%@", pattern, error)
+            return false
+        }
+        return matches(expression, matchingOptions: matchingOptions)
+    }
+    
+    public func matches(_ input: NSRegularExpression?, matchingOptions: NSRegularExpression.MatchingOptions = []) -> Bool {
+        guard let input = input else { return false }
+        let range = NSRange(location: 0, length: self.count)
+        return input.firstMatch(in: self, options: matchingOptions, range: range)?.range == range
+    }
+}
