6137f5852bfd35e47b5aa2581f23304c73c2312a
Nathan Jangula <njangula@myriadmobile.com>
2018-08-14 08:37:10 -0500

    Revert "Ghost file"
    This reverts commit b73a1801f2fff37ae53454d7d275d7c8ab8e08d1.

--- BetterKit/Classes/StringExtensions.swift
+++ BetterKit/Classes/StringExtensions.swift
@@ -1,27 +1,8 @@
 //
-//  StringExtensions.swift
+//  StringExtensions.swift
 //  BetterKit
 //
 //  Created by Jangle's MacBook Pro on 8/14/18.
 //
 
 import Foundation
-
-extension String {
-    func matches(pattern: String, options: NSRegularExpression.Options = [], matchingOptions: NSRegularExpression.MatchingOptions = []) -> Bool {
-        var expression: NSRegularExpression!
-        do {
-            expression = try NSRegularExpression(pattern: pattern, options: options)
-        } catch {
-            print("Invalid pattern: %@\n%@", pattern, error)
-            return false
-        }
-        return matches(expression, matchingOptions: matchingOptions)
-    }
-    
-    func matches(_ input: NSRegularExpression?, matchingOptions: NSRegularExpression.MatchingOptions = []) -> Bool {
-        guard let input = input else { return false }
-        let range = NSRange(location: 0, length: self.count)
-        return input.firstMatch(in: self, options: matchingOptions, range: range)?.range == range
-    }
-}
