3284c9824a97e93cc080efe3a3a6c426c570736d
Nathan Jangula <njangula@myriadmobile.com>
2018-08-22 13:00:38 -0500

    Added a DataExtensions class

---
+++ BetterKit/Classes/DataExtensions.swift
@@ -0,0 +1,29 @@
+//
+//  DataExtensions.swift
+//  BetterKit
+//
+//  Created by Jangle's MacBook Pro on 8/22/18.
+//
+
+import Foundation
+
+extension Data {
+    public struct HexEncodingOptions: OptionSet {
+        public let rawValue: Int
+        public static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
+        public init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
+    }
+    
+    public func hexEncodedString(options: HexEncodingOptions = [.upperCase]) -> String {
+        let hexDigits = Array((options.contains(.upperCase) ? "0123456789ABCDEF" : "0123456789abcdef").utf16)
+        var chars: [unichar] = []
+        chars.reserveCapacity(2 * count)
+        for byte in self {
+            chars.append(hexDigits[Int(byte / 16)])
+            chars.append(hexDigits[Int(byte % 16)])
+        }
+        return String(utf16CodeUnits: chars, count: chars.count)
+    }
+}
